2b456c159196490785cd868b6c8394dd9d0f4ba6
enyone <enyone@phnet.fi>
2012-11-06 16:44:29 +0000

    * fixed user registration backend * refactored login view

--- src/Enymind/Bundle/Health/SecureBundle/Entity/User.php
+++ src/Enymind/Bundle/Health/SecureBundle/Entity/User.php
@@ -48,7 +48,7 @@ class User implements UserInterface
 
     public function __construct()
     {
-        $this->isActive = 1;
+        $this->isActive = true;
         $this->salt = md5(uniqid(null, true));
         $this->email = "";
     }
