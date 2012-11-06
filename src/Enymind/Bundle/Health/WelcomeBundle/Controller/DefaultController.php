ec2914c927a9029f8cabe86794630a19d5f4be57
enyone <enyone@phnet.fi>
2012-11-06 21:41:56 +0000

    * refactoring

--- src/Enymind/Bundle/Health/WelcomeBundle/Controller/DefaultController.php
+++ src/Enymind/Bundle/Health/WelcomeBundle/Controller/DefaultController.php
@@ -45,7 +45,8 @@ class DefaultController extends Controller
         if( !empty( $username ) )
           $lastUsername = $username;
         
-        $this->get('session')->setFlash('warning', $this->get('translator')->trans('Error occured during log in process:') . " " . $error->message );
+        if( !empty( $error->message ) )
+          $this->get('session')->setFlash('warning', $this->get('translator')->trans('Error occured during log in process:') . " " . $error->message );
         
         return array(
           'last_username' => $lastUsername,
