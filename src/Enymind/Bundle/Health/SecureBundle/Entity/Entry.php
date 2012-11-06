b426cb1afc862600a9caf810711029d2910fbc3a
enyone <enyone@phnet.fi>
2012-11-06 19:52:41 +0000

    added entity Entry

---
+++ src/Enymind/Bundle/Health/SecureBundle/Entity/Entry.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Enymind\Bundle\Health\SecureBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Enymind\Bundle\Health\SecureBundle\Entity\Entry
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Entry
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer $type_id
+     *
+     * @ORM\Column(name="type_id", type="integer")
+     */
+    private $type_id;
+
+    /**
+     * @var integer $value
+     *
+     * @ORM\Column(name="value", type="integer")
+     */
+    private $value;
+
+    /**
+     * @var \DateTime $added
+     *
+     * @ORM\Column(name="added", type="datetime")
+     */
+    private $added;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type_id
+     *
+     * @param integer $typeId
+     * @return Entry
+     */
+    public function setTypeId($typeId)
+    {
+        $this->type_id = $typeId;
+    
+        return $this;
+    }
+
+    /**
+     * Get type_id
+     *
+     * @return integer 
+     */
+    public function getTypeId()
+    {
+        return $this->type_id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param integer $value
+     * @return Entry
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return integer 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set added
+     *
+     * @param \DateTime $added
+     * @return Entry
+     */
+    public function setAdded($added)
+    {
+        $this->added = $added;
+    
+        return $this;
+    }
+
+    /**
+     * Get added
+     *
+     * @return \DateTime 
+     */
+    public function getAdded()
+    {
+        return $this->added;
+    }
+}
