35caec0384a7129c5ed1d67ecae0e9a323100e5e
enyone <enyone@phnet.fi>
2012-11-06 16:21:30 +0000

    * fixed user registration backend

--- src/Enymind/Bundle/Health/SecureBundle/Entity/User.php
+++ src/Enymind/Bundle/Health/SecureBundle/Entity/User.php
@@ -37,7 +37,7 @@ class User implements UserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=128, unique=true)
+     * @ORM\Column(type="string", length=128)
      */
     private $email;
 
