95fed88973f960766637bd64aaa0dced5a8bd704
enyone <enyone@phnet.fi>
2012-11-08 15:06:50 +0000

    added EntryType CRUD

---
+++ src/Enymind/Bundle/Health/SecureBundle/Controller/EntryTypeController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Enymind\Bundle\Health\SecureBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Enymind\Bundle\Health\SecureBundle\Entity\EntryType;
+use Enymind\Bundle\Health\SecureBundle\Form\EntryTypeType;
+
+/**
+ * EntryType controller.
+ *
+ * @Route("/secure/manage/types")
+ */
+class EntryTypeController extends Controller
+{
+    /**
+     * Lists all EntryType entities.
+     *
+     * @Route("/", name="secure_manage_types")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('EnymindHealthSecureBundle:EntryType')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a EntryType entity.
+     *
+     * @Route("/{id}/show", name="secure_manage_types_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EnymindHealthSecureBundle:EntryType')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EntryType entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new EntryType entity.
+     *
+     * @Route("/new", name="secure_manage_types_new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new EntryType();
+        $form   = $this->createForm(new EntryTypeType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new EntryType entity.
+     *
+     * @Route("/create", name="secure_manage_types_create")
+     * @Method("POST")
+     * @Template("EnymindHealthSecureBundle:EntryType:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new EntryType();
+        $form = $this->createForm(new EntryTypeType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('secure_manage_types_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing EntryType entity.
+     *
+     * @Route("/{id}/edit", name="secure_manage_types_edit")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EnymindHealthSecureBundle:EntryType')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EntryType entity.');
+        }
+
+        $editForm = $this->createForm(new EntryTypeType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing EntryType entity.
+     *
+     * @Route("/{id}/update", name="secure_manage_types_update")
+     * @Method("POST")
+     * @Template("EnymindHealthSecureBundle:EntryType:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EnymindHealthSecureBundle:EntryType')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EntryType entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new EntryTypeType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('secure_manage_types_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a EntryType entity.
+     *
+     * @Route("/{id}/delete", name="secure_manage_types_delete")
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('EnymindHealthSecureBundle:EntryType')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find EntryType entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('secure_manage_types'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Enymind/Bundle/Health/SecureBundle/Form/EntryTypeType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Enymind\Bundle\Health\SecureBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EntryTypeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('min')
+            ->add('max')
+            ->add('quantity')
+            ->add('owner_id')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Enymind\Bundle\Health\SecureBundle\Entity\EntryType'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'enymind_bundle_health_securebundle_entrytypetype';
+    }
+}
