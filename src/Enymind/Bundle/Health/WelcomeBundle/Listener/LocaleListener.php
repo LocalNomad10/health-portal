b01c8b3964c9ef664caa6c4fe40234aecfddcbe8
enyone <enyone@phnet.fi>
2012-11-15 19:39:12 +0000

    * added locale listener

---
+++ src/Enymind/Bundle/Health/WelcomeBundle/Listener/LocaleListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Enymind\Bundle\Health\WelcomeBundle\Listener;
+
+class LocaleListener
+{
+    protected $container;
+    protected $availableLocales;
+
+    public function __construct(\Symfony\Component\DependencyInjection\Container $container, $availableLocales) {
+        $this->container = $container;
+        $this->availableLocales = $availableLocales;
+    }
+
+    public function onKernelRequest(GetResponseEvent $e) {
+        $req = $e->getRequest();
+        $locale = $req->getPreferredLanguage($this->availableLocales);
+        $session = $this->container->get('session');
+        $session->setLocale($locale);
+    }
+}
+
+?>
