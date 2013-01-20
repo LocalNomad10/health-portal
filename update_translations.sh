12cae15c17a2a0ad18188baf702fcae6245ebd7a
enyone <enyone@phnet.fi>
2013-01-20 18:56:51 +0200

    added build and translation scripts

---
+++ update_translations.sh
@@ -0,0 +1,6 @@
+php app/console translation:update en EnymindHealthSecureBundle --force --output-format=xlf
+php app/console translation:update en EnymindHealthWelcomeBundle --force --output-format=xlf
+
+php app/console translation:update fi EnymindHealthWelcomeBundle --force --output-format=xlf
+php app/console translation:update fi EnymindHealthSecureBundle --force --output-format=xlf
+
